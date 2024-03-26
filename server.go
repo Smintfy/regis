6221d05468e064574deb9e12dd07a906ba0f1587
Smintfy <hey@smintfy.com>
2024-03-26 14:27:22 +0700

    (update): handle concurrency

--- server.go
+++ server.go
@@ -20,27 +20,36 @@ func main() {
 	// close the listener once exit
 	defer listener.Close()
 
-	conn, err := listener.Accept()
-	if err != nil {
-		fmt.Println("Failed to bind to port: ", PORT)
-        os.Exit(1)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Failed to bind to port: ", PORT)
+			os.Exit(1)
+		}
+
+		// concurrency for handling multiple connection
+		go handleConnection(conn)
 	}
-	
+}
+
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	// Read data
-	buffer := make([]byte, 1024)
-	n, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading buffer:", err)
-		os.Exit(1)
+	for {
+		// read data
+		buffer := make([]byte, 1024)
+		n, err := conn.Read(buffer)
+		if err != nil {
+			fmt.Println("Error reading buffer:", err)
+			return
+		}
+
+		log.Printf("Received %d bytes", n)
+		log.Println("Received data: ", buffer[:n])
+		log.Println("Data: ", string(buffer[:n]))
+
+		// write data
+		msg := []byte("+PONG\r\n")
+		conn.Write(msg)
 	}
-
-	log.Printf("Received %d bytes", n)
-	log.Println("Received data: ", buffer[:n])
-	log.Println("Data: ", string(buffer[:n]))
-
-	// Write data
-	msg := []byte("+PONG\r\n")
-	conn.Write(msg)
 }
